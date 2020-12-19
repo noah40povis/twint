290617383f05288343513f68fcc111a2083245a4
Noah Christian <noahchristian@Noahs-MacBook-Pro.local>
2020-12-18 23:46:03 -0500

    issues with db transfer

--- automate.py
+++ automate.py
@@ -4,58 +4,58 @@ import time
 
 # you can change the name of each "job" after "def" if you'd like.
 def jobone():
-	print ("Fetching Tweets")
 	c = twint.Config()
-	# choose username (optional)
-	c.Username = "insert username here"
-	# choose search term (optional)
-	c.Search = "insert search term here"
-	# choose beginning time (narrow results)
-	c.Since = "2018-01-01"
-	# set limit on total tweets
-	c.Limit = 1000
-	# no idea, but makes the csv format properly
-	c.Store_csv = True
-	# format of the csv
-	c.Custom = ["date", "time", "username", "tweet", "link", "likes", "retweets", "replies", "mentions", "hashtags"]
-	# change the name of the csv file
-	c.Output = "filename.csv"
+	#tweets that contain these texts 
+	c.Search = 'raised series A'
+	c.Search = 'raised series B'
+	#timeframe 
+	c.Since = '2020-01-01'
+	#language of tweets 
+	c.Lang = "en"
+	#c.Custom = ["date", "time", "username", "tweet", "link", "likes", "retweets", "replies", "mentions", "hashtags", "location"]
+	c.Limit = 2000
+	#store to csv 
+	#c.Output = "datascience6.csv"
+	#c.Store_csv = True
+	#store in elastic search instance
+	c.Database = "datascience1.db"
+	#run
 	twint.run.Search(c)
 
-def jobtwo():
-	print ("Fetching Tweets")
-	c = twint.Config()
-	# choose username (optional)
-	c.Username = "insert username here"
-	# choose search term (optional)
-	c.Search = "insert search term here"
-	# choose beginning time (narrow results)
-	c.Since = "2018-01-01"
-	# set limit on total tweets
-	c.Limit = 1000
-	# no idea, but makes the csv format properly
-	c.Store_csv = True
-	# format of the csv
-	c.Custom = ["date", "time", "username", "tweet", "link", "likes", "retweets", "replies", "mentions", "hashtags"]
-	# change the name of the csv file
-	c.Output = "filename2.csv"
-	twint.run.Search(c)
+# def jobtwo():
+# 	print ("Fetching Tweets")
+# 	c = twint.Config()
+# 	# choose username (optional)
+# 	c.Username = "insert username here"
+# 	# choose search term (optional)
+# 	c.Search = "insert search term here"
+# 	# choose beginning time (narrow results)
+# 	c.Since = "2018-01-01"
+# 	# set limit on total tweets
+# 	c.Limit = 1000
+# 	# no idea, but makes the csv format properly
+# 	c.Store_csv = True
+# 	# format of the csv
+# 	c.Custom = ["date", "time", "username", "tweet", "link", "likes", "retweets", "replies", "mentions", "hashtags"]
+# 	# change the name of the csv file
+# 	c.Output = "filename2.csv"
+# 	twint.run.Search(c)
 
 # run once when you start the program
 
 jobone()
-jobtwo()
+#jobtwo()
 
 # run every minute(s), hour, day at, day of the week, day of the week and time. Use "#" to block out which ones you don't want to use.  Remove it to active. Also, replace "jobone" and "jobtwo" with your new function names (if applicable)
 
-# schedule.every(1).minutes.do(jobone)
-schedule.every().hour.do(jobone)
+schedule.every(1).minutes.do(jobone)
+#schedule.every().hour.do(jobone)
 # schedule.every().day.at("10:30").do(jobone)
 # schedule.every().monday.do(jobone)
 # schedule.every().wednesday.at("13:15").do(jobone)
 
 # schedule.every(1).minutes.do(jobtwo)
-schedule.every().hour.do(jobtwo)
+#schedule.every().hour.do(jobtwo)
 # schedule.every().day.at("10:30").do(jobtwo)
 # schedule.every().monday.do(jobtwo)
 # schedule.every().wednesday.at("13:15").do(jobtwo)
--- extract.py
+++ extract.py
@@ -1,11 +1,16 @@
 import twint 
 
 c = twint.Config()
-c.Search = 'data science role hiring'
+#tweets that contain these texts 
+c.Search = 'raised Series A'
+#timeframe 
+c.Since = '2020-01-01'
+#language of tweets 
 c.Lang = "en"
-c.Limit = 200
-c.Output = "datascience2.csv"
-c.Store_csv = True
-
+#store to csv 
+# c.Output = "datascience4.csv"
+# c.Store_csv = True
+#store in elastic search instance
+c.Elasticsearch = "http://127.0.0.1:9200/"
 #run
 twint.run.Search(c)
